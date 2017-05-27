0bae678018af09270a30b698ede5b4493e8ee116
jinghe.zhang <jinghe.zhang@veritas.com>
2017-05-27 17:17:16 +0800

    Added online document for closures

---
+++ closures/Closure.js
@@ -0,0 +1,5 @@
+/*
+* Refer the online training documents:
+* https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Closures For Chinese
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures For English
+*/
