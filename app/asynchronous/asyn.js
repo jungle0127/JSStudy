09865a2c50dcb84fdca29b4501b9d540efcd030f
jungle0127 <arbor@foxmail.com>
2017-07-11 23:41:12 +0800

    added jquery and q library

--- app/asynchronous/asyn.js
+++ app/asynchronous/asyn.js
@@ -1,8 +1,10 @@
 /**
  * http://usejsdoc.org/
  * A good article for asynchronous JS:
- * https://code.tutsplus.com/tutorials/event-based-programming-what-async-has-over-sync--net-30027 
+ * https://code.tutsplus.com/tutorials/event-based-programming-what-async-has-over-sync--net-30027
+ * http://blog.codingplayboy.com/2017/04/25/js_async/ 
  */
+var jquery = require('../../node_modules/jquery/dist/jquery.min.js');
 console.log('a');
 setTimeout(function() {
 	console.log('c');
@@ -14,3 +16,5 @@ setTimeout(function(){
 	console.log('e');
 },500);
 console.log('b');
+
+console.error('adb');
