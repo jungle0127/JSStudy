e2134986c1f5b06deebf2020a44129c2027eb9f1
jungle0127 <arbor@foxmail.com>
2018-11-08 22:36:15 +0800

    update official demo

--- app/Angular4/officialdemo/angular-tour-of-heros/src/app/heroes/heroes.component.ts
+++ app/Angular4/officialdemo/angular-tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero} from '../hero'
+import { heroStore} from '../mock-heros'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  heros  = heroStore;
   constructor() { }
 
   ngOnInit() {
---
+++ app/Angular4/officialdemo/angular-tour-of-heros/src/app/mock-heros.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero';
+
+export const heroStore: Hero[] = [
+{id: 1, name: "Mr. Nice"},
+{id: 2, name: "Naro"},
+{id: 3, name: "Bombasto"}
+];
